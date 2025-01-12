0dd0fc15d69300e8587b11ffc862dcef9a99363a
mayageva2 <mayageva7@gmail.com>
2025-01-12 10:32:16 +0200

    fixed screen choise according to name

--- menu.cpp
+++ menu.cpp
@@ -51,9 +51,8 @@ void Menu::displayMenu(Mario& mario) // displays main menu
 			while (!_kbhit()) {}
 			break;
 		case '2':
-			printScreen(chooseScreen);
-			screenKey = _getch();
-			choosescreen(screenKey, screens, board, screenError, mario);
+			screenKey = printChooseScreen(screens);
+			loadChosenScreen(screenKey, screens, board, screenError, mario);
 			break;
 		case '9':
 			exitMenu = true;
@@ -62,21 +61,78 @@ void Menu::displayMenu(Mario& mario) // displays main menu
 	}
 }
 
-void Menu::choosescreen(char& screenKey, std::vector<std::string>& screens, GameConfig& board, bool& screenError, Mario& mario)
+char Menu::printChooseScreen(std::vector<std::string> screens)
 {
-	switch (screenKey)
+	size_t size = screens.size();
+	int i = 0, j = 0;
+	char counter = '1';
+	while (size != 0)
 	{
-	case '1':
-		loadScreens(0, screens, board, screenError, mario);
-		break;
-	case '2':
-		loadScreens(1, screens, board, screenError, mario);
-		break;
-	case '3':
-		loadScreens(2, screens, board, screenError, mario);
-		break;
+		switch (size)
+		{
+		case 1: {
+			Point namePoints[1] = { Point(38, 16) };
+			Point counterPoints[1] ={ Point(49, 18) }; //check place
+			j = 0;
+			printScreen(choose1Screen);
+			addNames(1, counter, namePoints, counterPoints, screens);
+			size--;
+			break;
+		}
+		case 2: {
+			Point namePoints[2] = { Point(20, 16), Point(56, 16) }; //check place
+			Point counterPoints[2] = { Point(31, 18),  Point(67, 18) }; //check place
+			j = 0;
+			printScreen(choose2Screen);
+			addNames(2, counter, namePoints, counterPoints, screens);
+			size = size - 2;
+			break;
+		}
+		default: {
+			Point namePoints[3] = { Point(13, 16), Point(38, 16), Point(63, 16) };
+			Point counterPoints[3] = { Point(24, 18), Point(49, 18), Point(74, 18) }; //check place
+			j = 0;
+			printScreen(choose3Screen);
+			namePoints[i].draw(screens[i][j], namePoints[i]);
+			addNames(3, counter, namePoints, counterPoints, screens);
+			size = size - 3;
+			break;
+		}
+		}
+		if (size != 0)
+		{
+			gotoxy(61, 20);
+			std::cout << "press 0 to next screens";
+		}
+		char key = _getch();
+		if (key != '0')
+			return key;
 	}
 }
+
+void Menu::addNames(int size, char& counter, Point* namePoints, Point* counterPoints, std::vector<std::string>& screens)
+{
+	int j = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		while (screens[i][j] != '\0')
+		{
+			namePoints[i].draw(screens[i][j], namePoints[i]);
+			namePoints[i].x++;
+			j++;
+		}
+		counterPoints[i].draw(counter, counterPoints[i]);
+		counter++;
+		j = 0;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		screens.erase(screens.begin());
+	}
+}
+
 
 void Menu::loadScreens(size_t i, std::vector<std::string>& screens, GameConfig& board, bool& screenError, Mario& mario)
 {
@@ -98,6 +154,12 @@ void Menu::loadScreens(size_t i, std::vector<std::string>& screens, GameConfig& 
 		}
 	}
 	mario.resetMario();
+}
+
+void Menu::loadChosenScreen(char& screenKey, std::vector<std::string>& screens, GameConfig& board, bool& screenError, Mario& mario)
+{
+	int i = screenKey - '0' -1;
+	loadScreens(i, screens, board, screenError, mario);
 }
 
 void Menu::displayEnd_Game(Mario& mario)  //ends game
